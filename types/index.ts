b907530eedff55060c304b61dfd6e7edb3c9e162
gmitsolution3 <gmitsolution3@gmail.com>
2026-05-20 15:40:16 +0600

    feat: admin dashboard - slider page added

--- types/index.ts
+++ types/index.ts
@@ -5,3 +5,4 @@ export * from "./team-member.type";
 export * from "./blog.type";
 export * from "./case-study.type";
 export * from "./job-posting.type";
+export * from "./slider.type";
---
+++ types/slider.type.ts
@@ -0,0 +1,11 @@
+export interface ISlider {
+  _id: string;
+  title: string;
+  description: string;
+  highlight: string;
+  ctaPrimary: string;
+  ctaSecondary: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+}
